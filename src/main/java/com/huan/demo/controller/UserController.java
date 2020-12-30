76afb71f27061b5787551f3db10dc7403a8478a3
mubaisama <2720768262@qq.com>
2020-12-31 00:12:31 +0800

    redis

--- src/main/java/com/huan/demo/controller/UserController.java
+++ src/main/java/com/huan/demo/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.huan.demo.controller;
 
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,9 +17,21 @@ import org.springframework.web.bind.annotation.RestController;
  * @author MuBaiSama
  * @since 2020-12-15
  */
+@Data
 @RestController
 @RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
+
+
+    private final StringRedisTemplate redisTemplate;
+
+    @GetMapping("/getOne")
+    String getOne(){
+        ValueOperations<String,String> operations = redisTemplate.opsForValue();
+        operations.set("hello","world");
+        return operations.get("hello");
+    }
 
 }
 
